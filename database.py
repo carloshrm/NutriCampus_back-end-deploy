07f7dee148da88bb63b6c57cc49aff43681823af
Carlos Moraes <carloshrmoraes@gmail.com>
2024-10-03 10:30:38 -0300

    remove print

--- database.py
+++ database.py
@@ -12,9 +12,6 @@ host = os.getenv("HOST")
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}/{database}"
 
-print(SQLALCHEMY_DATABASE_URL)
-
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
