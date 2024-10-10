e29eb801bc2f6d2f8effba09e2b93d0503eca117
Carlos Moraes <carloshrmoraes@gmail.com>
2024-10-10 11:29:55 -0300

    add cors

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from fastapi import Depends, FastAPI
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI
 from database import Base, engine
 from scraping import timer
 from routers import alimento_route, usuario_route, cardapio_route
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 timer.setup_scrape_jobs()
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(usuario_route.router)
 app.include_router(alimento_route.router)
