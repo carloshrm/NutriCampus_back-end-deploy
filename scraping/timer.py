221df9c5c22c4a4605efdb41cbd30806158a24e2
Jonathan Borges <mborges.jonathan@gmail.com>
2024-11-08 09:56:38 -0300

    adiciona outros chamadas de scraping no timer, e deleta arquivo

--- scraping/timer.py
+++ scraping/timer.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 from apscheduler.schedulers.background import BackgroundScheduler
 from scraping.TACO import script as TACOScraping
 from scraping.Cardapio import scrapping as CardapioScraping
+from scraping.receitas import prato as PratoMapeamento
+from scraping.ingredientes import main as IngredientesteScraping
+#from scraping.ingredientes import verificar_ingredientes as GeminiAction
 
 load_dotenv()
 
@@ -18,12 +21,30 @@ JOB_NAME = 'z-timer'
 def run_all_scrapes():
   print("Rodando todos os scrapes")
   try:
+    print("Rodando TACOScraping.executar_scraping()")
     TACOScraping.executar_scraping()
   except:
     print("Erro ao executar scraping da tabela TACO.")
 
-  CardapioScraping.main()
-  # chamar outras tarefas de scraping
+  try:
+    print("Rodando CardapioScraping.main()")
+    CardapioScraping.main()
+  except:
+    print("Erro ao executar scraping de cardapio da UFU")
+
+  try:
+    print("Rodando mapeamento_prato()")
+    PratoMapeamento.main()
+  except:
+    print("Erro ao executar mapeamento dos prato baseado em Cardapios")
+    
+  try:
+    print("Rodando ingredientesScraping main()")
+    IngredientesteScraping.main()
+    #GeminiAction.processar_ingredientes_com_gemini()
+  except:
+    print("Erro ao executar ingredientesScraping main()")  
+
 
 def setup_scrape_jobs():
   scheduler = BackgroundScheduler()
@@ -33,4 +54,3 @@ def setup_scrape_jobs():
   if not scheduler.get_job(JOB_NAME):
     run_all_scrapes()
     scheduler.add_job(run_all_scrapes, 'cron', day_of_week='sun', hour=1, id=JOB_NAME, coalesce=True)
-  
